0b809cf3d501f815f2f51bcede6f17d9cd77189c
Bryan Cross <bryancross@github.com>
2017-03-31 14:51:41 -0500

    Final boostrapping

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -5,5 +5,13 @@ mkdir -p log
 echo "Creating jobs directory"
 mkdir -p jobs
 cp config/job-template-example.json config/job-template.json
-echo "Remember to edit config/job-template.json and, optionally,"
-echo "test/test-params.json with correct default values"
+cp config/config-example.json config.json
+clear
+echo "****************************************************************"
+echo
+echo "Remember to edit config/job-template.json and config/config.json"
+echo "Add your PATs and ensure the values make sense for your environment"
+echo
+echo "****************************************************************"
+
+
